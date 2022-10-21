a1fb3770ffd0353ac60b6d91219edfc0a5b914ff
Sara Youssef <sarary2000@gmail.com>
2022-10-21 15:30:14 +0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  //// your code here
+
+  const input = document.getElementById("new-task-description");
+  const formElement = document.querySelector("form");
+  formElement.addEventListener('submit', (event) => {
+  let task =input.value
+
+    event.preventDefault();
+    if (task.length===0){
+    alert('Task cannot be empty')
+    }else{
+      ToDo(task);
+    }
+    formElement.reset()
+  });
 });
+
+
+function css(element, style) {
+  for (const property in style)
+      element.style[property] = style[property];
+}
+
+function ToDo (task) {
+  let listItem = document.createElement("li");
+  let btn = document.createElement('button')
+ 
+  css(btn, {  
+'background' :'red',
+'padding' : '10px',
+'margin' : '2px',
+'font-size' :'15px',
+});
+
+  btn.addEventListener('click',ToDelete)
+  btn.textContent='X delete'
+  listItem.innerText = `${task}`;
+  listItem.appendChild(btn)
+  document.querySelector("#tasks").appendChild(listItem);
+}
+
+function ToDelete(event){
+event.target.parentNode.remove()
+}
